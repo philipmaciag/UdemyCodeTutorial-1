4005b51cd43456f5474975f1ce03728ab0bb7d82
philipmaciag <philip.maciag@hotmail.com>
2021-09-04 14:22:05 +0200

    Init

---
+++ udemy_course training/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace udemy_course_training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //main 
+            int num1 = 10;
+            string i = num1.ToString();
+            Console.WriteLine(i);
+            string s1 = "1";
+            int num2 = Int32.Parse(s1);
+            Console.WriteLine(num2);
+
+        }
+    }
+}
