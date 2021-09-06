baf17865d3c06ed358861af9c62317b4e98049e1
philipmaciag <philip.maciag@hotmail.com>
2021-09-06 20:49:35 +0200

    changes

--- udemy_course training/Program.cs
+++ udemy_course training/Program.cs
@@ -2,23 +2,43 @@
 
 
 namespace Udemy_course {
+static class Extend
+    {
 
+        public static string CapitaliseFirst(this string x)
+        {
+            var firstLetterOfFirstName = x.Substring(0, 1);
+            firstLetterOfFirstName = firstLetterOfFirstName.ToUpper();
+            var restOfLettersInFirstName = x.Substring(1);
+            var BetterFirstName = firstLetterOfFirstName + restOfLettersInFirstName;
+            return BetterFirstName;
+        }
+
+
+
+
+    }
 class Program {
         
         static void Main(string[] args) {
            
-            Console.WriteLine("Write your name and press enter.");
-            var Name = Console.ReadLine();
-            
-            Console.WriteLine(Name.ToUpper());
-            Console.WriteLine(Name.ToLower());
-            Console.WriteLine(Name.Trim());
-            Console.WriteLine(Name.Substring(2));
+            Console.WriteLine("Write a String and press enter.");
+            var mystring = Console.ReadLine();
+            Console.WriteLine("enter a character to search");
+            char my_second_string = (char)Console.Read();
+            Console.ReadLine();
+            Console.WriteLine(mystring.IndexOf(my_second_string));
+            Console.WriteLine("Enter your first Name");
+            var firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last Name");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine(firstName.CapitaliseFirst());
+            Console.WriteLine(lastName.CapitaliseFirst());
             Console.Read();
-
-
-
         }
+
+        
     
     };
 
